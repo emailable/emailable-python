69b96d84c12feec54f8d0d77c75a8192c98cb7c9
evan berquist <evan@cacheventures.com>
2020-04-01 09:59:57 -0400

    update setup. add version and travis file

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
 
 setuptools.setup(
     name="blazeverify",
-    version="0.0.1",
-    author="Evan Berquist",
-    author_email="evan@cacheventures.com",
+    version=version,
+    author="Blaze Verify",
+    author_email="support@blazeverify.com",
     description="This is the official python wrapper for the Blaze Verify API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/blazeverify/blazeverify-python",
+    license="MIT",
+    keywords="blazeverify email verification",
     packages=setuptools.find_packages(),
     install_requires = [
         'requests'
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
+    project_urls={
+        "Bug Tracker": "https://github.com/blazeverify/blazeverify-python/issues",
+        "Documentation": "https://docs.blazeverify.com/?python",
+        "Source Code": "https://github.com/blazeverify/blazeverify-python",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
